5a3f292b9b7e9efca96c69f15cefb25140e19b27
Nevera05 <gmneves05@gmail.com>
2023-04-09 20:08:09 -0300

    Botão

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import RandomGenerator from './components/RandomGenerator';
+import { View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <RandomGenerator min={1} max={10} />
+    <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexGrow: 1}}>
+      <View style={{marginRight: 20}}>
+        <Button title='/' />
+        <Button title="7" />
+        <Button title="4" />
+        <Button title="1" />
+      </View>
+      <View style={{marginRight: 20}}>
+        <Button title='+' />
+        <Button title="8" />
+        <Button title="5" />
+        <Button title="2" />
+      </View>
+      <View>
+        <Button title='*' />  
+        <Button title="9" />
+        <Button title="6" />
+        <Button title="3" />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
